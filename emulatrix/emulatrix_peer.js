8b987662371e78bd9f1985307b67db4e039758c0
Sergei Vasilev <sergen-vas@yandex.ru>
2021-12-11 22:50:11 +0300

    Add files via upload

--- emulatrix/emulatrix_peer.js
+++ emulatrix/emulatrix_peer.js
@@ -1,61 +1,86 @@
 var peer = new Peer();
+var peerIds = {
+	a: null,
+	b: null,
+}
+
+closea.onclick = function () {
+	closea.closest('#qrcode').remove();
+}
+closeb.onclick = function () {
+	closeb.closest('#qrcodeb').remove();
+}
+
 peer.on('open', function(id) {
 	drawQR(id);
 
 	peer.on('connection', function(conn) {
-		document.getElementById("qrcode1").hidden = true;
 
 		conn.on('open', function() {
-			console.log('open');
 
 			// Receive messages
 			conn.on('data', function(state) {
-				console.log('Received', state);
+				if (state.id) {
+					if (state.id === 'a') {
+						peerIds.a = conn.peer;
+						document.getElementById("qrcode1").hidden = true;
+					}
+					else {
+						peerIds.b = conn.peer;
+						document.getElementById("qrcodeb1").hidden = true;
+					}
 
-				if (state.fire) {
-					sendVirtualKey("keydown","KeyZ");
-					setTimeout(function () {
-						sendVirtualKey("keyup","KeyZ");
-					}, 25);
+					return;
 				}
 
-				if (state.alter) {
-					sendVirtualKey("keydown","KeyX");
-					setTimeout(function () {
-						sendVirtualKey("keyup","KeyX");
-					}, 25);
+				if (conn.peer === peerIds.a) {
+					if (state.fire) {
+						sendVirtualKey("keydown","KeyZ");
+						setTimeout(function () {
+							sendVirtualKey("keyup","KeyZ");
+						}, 25);
+					}
+
+					if (state.alter) {
+						sendVirtualKey("keydown","KeyX");
+						setTimeout(function () {
+							sendVirtualKey("keyup","KeyX");
+						}, 25);
+					}
+
+					sendVirtualKey(state.up ? 'keydown' : 'keyup', 'ArrowUp');
+					sendVirtualKey(state.right ? 'keydown' : 'keyup', 'ArrowRight');
+					sendVirtualKey(state.down ? 'keydown' : 'keyup', 'ArrowDown');
+					sendVirtualKey(state.left ? 'keydown' : 'keyup', 'ArrowLeft');
 				}
 
-				sendVirtualKey(state.up ? 'keydown' : 'keyup', 'ArrowUp');
-				sendVirtualKey(state.right ? 'keydown' : 'keyup', 'ArrowRight');
-				sendVirtualKey(state.down ? 'keydown' : 'keyup', 'ArrowDown');
-				sendVirtualKey(state.left ? 'keydown' : 'keyup', 'ArrowLeft');
+				if (conn.peer === peerIds.b) {
+					if (state.fire) {
+						sendVirtualKey("keydown","KeyI");
+						setTimeout(function () {
+							sendVirtualKey("keyup","KeyI");
+						}, 25);
+					}
+
+					if (state.alter) {
+						sendVirtualKey("keydown","KeyU");
+						setTimeout(function () {
+							sendVirtualKey("keyup","KeyU");
+						}, 25);
+					}
+
+					sendVirtualKey(state.up ? 'keydown' : 'keyup', 'KeyQ');
+					sendVirtualKey(state.right ? 'keydown' : 'keyup', 'KeyR');
+					sendVirtualKey(state.down ? 'keydown' : 'keyup', 'KeyW');
+					sendVirtualKey(state.left ? 'keydown' : 'keyup', 'KeyE');
+				}
 			});
-			// conn.on('data', function(state) {
-			// 	// console.log('Received', state);
-
-			// 	if (state.fire) {
-			// 		sendVirtualKey("keydown","KeyI");
-			// 		setTimeout(function () {
-			// 			sendVirtualKey("keyup","KeyI");
-			// 		}, 25);
-			// 	}
-
-			// 	if (state.alter) {
-			// 		sendVirtualKey("keydown","KeyU");
-			// 		setTimeout(function () {
-			// 			sendVirtualKey("keyup","KeyU");
-			// 		}, 25);
-			// 	}
-
-			// 	sendVirtualKey(state.up ? 'keydown' : 'keyup', 'KeyQ');
-			// 	sendVirtualKey(state.right ? 'keydown' : 'keyup', 'KeyR');
-			// 	sendVirtualKey(state.down ? 'keydown' : 'keyup', 'KeyW');
-			// 	sendVirtualKey(state.left ? 'keydown' : 'keyup', 'KeyE');
-			// });
 		});
 
-		omg.onclick = function () {
+		omg.onclick = startStream;
+		omgb.onclick = startStream;
+
+		function startStream () {
 			var c = document.createElement('canvas');
 			var cx = c.getContext('2d');
 
@@ -79,10 +104,14 @@ peer.on('open', function(id) {
 
 function drawQR (id) {
 	var wrapper = document.getElementById("qrcode");
-	var url = '../lol.html' +'?peer='+ id;
+	var url = '../lol.html' +'?peer='+ id +'#a';
+	var a = document.createElement('a');
+
+	a.href = url;
+	a.innerHTML = url;
 
 	var qrcode = new QRCode(document.getElementById("qrcode1"), {
-		text: url,
+		text: a.href,
 		width: 256,
 		height: 256,
 		colorDark : "#000000",
@@ -90,9 +119,33 @@ function drawQR (id) {
 		correctLevel : QRCode.CorrectLevel.H
 	});
 
+	wrapper.appendChild(a);
+
 	wrapper.insertAdjacentHTML('beforeend', `
-		<a href="${url}">${url}</a>
 		<button id="omg">omg</button>
+	`);
+
+
+	var wrapper = document.getElementById("qrcodeb");
+	var url = '../lol.html' +'?peer='+ id +'#b';
+	var a = document.createElement('a');
+
+	a.href = url;
+	a.innerHTML = url;
+
+	var qrcode = new QRCode(document.getElementById("qrcodeb1"), {
+		text: a.href,
+		width: 256,
+		height: 256,
+		colorDark : "#000000",
+		colorLight : "#ffffff",
+		correctLevel : QRCode.CorrectLevel.H
+	});
+
+	wrapper.appendChild(a);
+
+	wrapper.insertAdjacentHTML('beforeend', `
+		<button id="omgb">omg</button>
 	`);
 
 	// omg.onclick = function () {
